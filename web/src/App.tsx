efccb4ff33a4819bfa3b86cb88dc2f99dfdcfa00
Ivan Vieira <ivanjr.ce@gmail.com>
2020-10-12 11:34:28 -0300

    Create Landing Page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import "./styles/global.css";
+import "./styles/pages/landing.css";
+import logoImg from "./images/logo.svg";
+import {FiArrowRight} from "react-icons/fi";
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Happy</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Leve Felicidade para o Mundo</h1>
+          <p>Visite Orfanatos e mude o dia de muitas Crianças</p>
+        </main>
+
+        <div className="location">
+          <strong>Fortaleza</strong>
+          <span>Ceará</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0,0,0,0.6)"/>
+        </a>
+      </div>
+
     </div>
   );
 }
