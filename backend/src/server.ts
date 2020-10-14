e8c9bb4907c5d89af0562136de5ab82ef48e5bb7
Ivan Vieira <ivanjr.ce@gmail.com>
2020-10-14 11:48:18 -0300

    Create Api Errors handling

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,16 @@
 import express from "express";
+import "express-async-errors";
 import path from "path";
 import "./database/connection";
 import routes from "./routes"
+import errorHandler from "./errors/handler";
 
 const server = express();
 
 server.use(express.json());
 server.use(routes);
 server.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+server.use(errorHandler);
 
 server.listen(3333);
 
