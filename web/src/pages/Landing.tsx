9a1da96a62ae0f8fdbc68d5239de4629c19eb4b1
Ivan Vieira <ivanjr.ce@gmail.com>
2020-10-14 15:18:07 -0300

    Update Landing.tsx

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "../styles/pages/landing.css";
 import logoImg from "../images/logo.svg";
-import {FiArrowRight} from "react-icons/fi";
-import {Link} from "react-router-dom";
+import { FiArrowRight } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 function Landing() {
-  return(
-<div id="page-landing">
+  return (
+    <div id="page-landing">
       <div className="content-wrapper">
-        <img src={logoImg} alt="Happy"/>
+        <img src={logoImg} alt="Happy" />
 
         <main>
           <h1>Leve Felicidade para o Mundo</h1>
@@ -21,10 +21,9 @@ function Landing() {
         </div>
 
         <Link to="/app" className="enter-app">
-          <FiArrowRight size={26} color="rgba(0,0,0,0.6)"/>
+          <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
         </Link>
       </div>
-
     </div>
   );
 }
