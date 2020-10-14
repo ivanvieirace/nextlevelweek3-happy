abe2508673e668f3798f1df877f45d656e421d10
Ivan Vieira <ivanjr.ce@gmail.com>
2020-10-13 21:11:41 -0300

    Create Orphanage Model & Feature new Orphanage

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,6 @@
 import express, { request, response } from "express";
+import {getRepository} from "typeorm";
+import Orphanage from "./models/Orphamage";
 import "./database/connection";
 
 const server = express();
@@ -7,6 +9,30 @@ server.use(express.json());
 
 server.listen(3333);
 
-server.get("/users", (request, response) => {
-  return response.json({message: "Server Backend API no Ar."});
+server.post("/orphanages", async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  });
+
+  await orphanagesRepository.save(orphanage);
+  
+  return response.status(201).json(orphanage);
 })
